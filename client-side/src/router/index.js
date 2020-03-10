48a3cb894e0178ea150869a972196218473ef9ee
Izabella Ribeiro Pinto <bellarpjf@gmail.com>
2020-03-10 16:01:01 -0300

    Configurações iniciais do model, service, view e router

--- client-side/src/router/index.js
+++ client-side/src/router/index.js
@@ -1,22 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Detail from '../views/Detail.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/register',
+    component: Register
+  },
+  {
+    path: '/profile',
+    name: 'profile',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    component: Detail
   }
 ]
 
---
+++ client-side/src/views/Detail.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: 'detail',
+    }
+</script>
+
+<style scoped>
+</style>
